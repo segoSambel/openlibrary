6c30974631b6dabcb121303f6c5dacbf5ea6197c
segoSambel <habibhusain313@gmail.com>
2024-04-08 22:52:25 +0700

    update editorconfig

--- src/application/database.ts
+++ src/application/database.ts
@@ -2,38 +2,38 @@ import {PrismaClient} from "@prisma/client";
 import {logger} from "./logging";
 
 export const prismaClient = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-    {
-      emit: "event",
-      level: "info",
-    },
-    {
-      emit: "event",
-      level: "warn",
-    },
-    {
-      emit: "event",
-      level: "error",
-    }
-  ],
+    log: [
+        {
+            emit: "event",
+            level: "query",
+        },
+        {
+            emit: "event",
+            level: "info",
+        },
+        {
+            emit: "event",
+            level: "warn",
+        },
+        {
+            emit: "event",
+            level: "error",
+        }
+    ],
 });
 
 prismaClient.$on("query", (event) => {
-  logger.info(event);
+    logger.info(event);
 })
 
 prismaClient.$on("info", (event) => {
-  logger.info(event);
+    logger.info(event);
 })
 
 prismaClient.$on("warn", (event) => {
-  logger.warn(event);
+    logger.warn(event);
 })
 
 prismaClient.$on("error", (event) => {
-  logger.error(event);
+    logger.error(event);
 })
--- src/application/logging.ts
+++ src/application/logging.ts
@@ -1,11 +1,11 @@
 import winston from "winston";
 
 export const logger = winston.createLogger(
-  {
-    level: "debug",
-    format: winston.format.json(),
-    transports: [
-      new winston.transports.Console({})
-    ]
-  }
+    {
+        level: "debug",
+        format: winston.format.json(),
+        transports: [
+            new winston.transports.Console({})
+        ]
+    }
 )
